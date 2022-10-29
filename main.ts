7058fe1fab8161d1f0e3caad44bf8a0fefd6a5c8
SeeeJay <113055585+SeeeJay@users.noreply.github.com>
2022-10-28 21:23:37 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,27 @@
 function right () {
     pins.servoSetPulse(AnalogPin.P8, 1700)
-    pins.servoSetPulse(AnalogPin.P13, 0)
+    pins.servoSetPulse(AnalogPin.P13, 1700)
     control.waitMicros(20000)
 }
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        forward()
+    }
+    if (receivedNumber == 2) {
+        backwards()
+    }
+    if (receivedNumber == 3) {
+        left()
+    }
+    if (receivedNumber == 4) {
+        right()
+    }
+    if (receivedNumber == 5) {
+        stop()
+    }
+})
 function left () {
-    pins.servoSetPulse(AnalogPin.P8, 0)
+    pins.servoSetPulse(AnalogPin.P8, 1300)
     pins.servoSetPulse(AnalogPin.P13, 1300)
     control.waitMicros(20000)
 }
@@ -13,31 +30,11 @@ function stop () {
     pins.servoSetPulse(AnalogPin.P13, 0)
     control.waitMicros(20000)
 }
-input.onButtonPressed(Button.A, function () {
-    forward()
-    for (let index = 0; index < 4; index++) {
-        sensor()
-        if (distance <= 4) {
-            num += 1
-        }
-        if (num <= 4) {
-            stop()
-            control.waitMicros(20000)
-            forward()
-        }
-    }
-})
-radio.onReceivedString(function (receivedString) {
-	
-})
 function backwards () {
     pins.servoSetPulse(AnalogPin.P8, 1300)
     pins.servoSetPulse(AnalogPin.P13, 1700)
     control.waitMicros(20000)
 }
-input.onButtonPressed(Button.B, function () {
-    right()
-})
 function forward () {
     pins.servoSetPulse(AnalogPin.P8, 1700)
     pins.servoSetPulse(AnalogPin.P13, 1300)
@@ -51,12 +48,32 @@ function sensor () {
     pins.digitalWritePin(DigitalPin.P1, 0)
     distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
 }
-let num = 0
 let distance = 0
 basic.showIcon(IconNames.Heart)
+radio.setGroup(11)
 distance = 0
-num = 0
+let count = 0
 basic.forever(function () {
-    sensor()
     basic.showNumber(distance)
+    for (let index = 0; index < 4; index++) {
+        sensor()
+        if (distance <= 4) {
+            count += 1
+        }
+        if (count == 4) {
+            stop()
+            control.waitMicros(500000)
+            backwards()
+            control.waitMicros(500000)
+            stop()
+            control.waitMicros(500000)
+            left()
+            control.waitMicros(500000)
+            stop()
+            control.waitMicros(500000)
+            forward()
+            count = 0
+        }
+    }
+    count = 0
 })
